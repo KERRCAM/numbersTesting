ae00cfbbf7ec30b24e99b71d9fb75e3f1c2fe97b
KERRCAM <kerrcameron@hotmail.com>
2021-11-18 16:12:27 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/numbersTesting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numbersTesting.dir/main.cpp.o"
+  "numbersTesting"
+  "numbersTesting.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numbersTesting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+cout << "numbers testing:" << endl;
+
+    return 0;
+}
