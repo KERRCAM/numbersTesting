d1a6467c2dc17798805daca50b84fedc67e97d16
KERRCAM <kerrcameron@hotmail.com>
2021-11-29 13:34:45 +0000

    numbers

--- main.cpp
+++ main.cpp
@@ -12,6 +12,16 @@ int main() {
     cout << 5 * 7 << endl;
     cout << 17 / 4 << endl;
     cout << 15 % 4 << endl;
+    cout << 4 + 5 * 10 << endl; //follows bidmas
+
+    int num1 = 5;
+    double num2 = 5.5;
+    num1++;  // plus 1
+    cout << num1 << endl;
+    num1 += 80; // adds 80 ( can replace + with other operators)
+    cout << num2 + 9 << endl;
+    cout << 10.0 / 3 << endl; // one has to be double to get decimal answer
+
 
 
     return 0;
