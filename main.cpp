cd434105ea2816fbff19ebb76dc6363d404175c2
KERRCAM <kerrcameron@hotmail.com>
2021-11-29 13:29:13 +0000

    numbers

--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@ using namespace std;
 int main() {
 
     cout << "numbers testing:" << endl;
-    cout << 40;
+    cout << 40 << endl;
+    cout << 348.8284 << endl;
+    cout << 5 + 7 << endl;
+    cout << 5 - 7 << endl;
+    cout << 5 * 7 << endl;
+    cout << 17 / 4 << endl;
+    cout << 15 % 4 << endl;
+
 
     return 0;
 }
